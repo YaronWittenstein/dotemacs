8c5ab134d2584e178a6308c2633b930939bb1f8e
Yaron Wittenstein <yaron.wittenstein@gmail.com>
2020-05-24 11:22:18 +0300

    config-rust edit

--- config/config-go.el
+++ config/config-go.el
@@ -3,6 +3,6 @@
 (with-eval-after-load 'go-mode
   (add-hook 'before-save-hook 'gofmt-before-save))
 
-(define-key evil-normal-state-map (kbd "t") 'godoc-at-point)
+;; (define-key evil-normal-state-map (kbd "t") 'godoc-at-point)
 
 (provide 'config-go)
--- config/config-rust.el
+++ config/config-rust.el
@@ -6,23 +6,29 @@
   (add-hook 'rust-mode-hook 'cargo-minor-mode)
   (add-hook 'rust-mode-hook 'smartparens-mode)
 
+  ;; identation will use spaces.
+  (add-hook 'rust-mode-hook
+          (lambda () (setq indent-tabs-mode nil)))
+
   ;; treat underscore as part of a word
   (add-hook 'rust-mode-hook '(lambda () (modify-syntax-entry ?_ "w"))))
 
 ;; lsp
 (with-eval-after-load 'lsp-mode
-  (add-hook 'rust-mode-hook #'lsp))
+  (add-hook 'rust-mode-hook #'lsp)
+  (setq lsp-rust-server 'rls)
+  (setq lsp-rust-rls-command '("rls")))
 
 (define-key evil-normal-state-map (kbd "t") 'lsp-find-definition)
 (define-key evil-normal-state-map (kbd "r") 'lsp-find-references)
 
 ;; cargo
-(setq cargo-process--command-clean "+nightly clean")
-(setq cargo-process--command-build "+nightly build")
-(setq cargo-process--command-run "+nightly run")
-(setq cargo-process--command-test "+nightly test -- --nocapture")
-(setq cargo-process--command-current-test "+nightly test -- --nocapture")
-(setq cargo-process--command-current-file-tests "+nightly test -- --nocapture")
+;; (setq cargo-process--command-clean "+nightly clean")
+;; (setq cargo-process--command-build "+nightly build")
+;; (setq cargo-process--command-run "+nightly run")
+;; (setq cargo-process--command-test "+nightly test -- --nocapture")
+;; (setq cargo-process--command-current-test "+nightly test -- --nocapture")
+;; (setq cargo-process--command-current-file-tests "+nightly test -- --nocapture")
 
 ;; (define-key cargo-minor-mode-map (kbd "C-t") 'cargo-process-current-test)
 ;; (define-key cargo-minor-mode-map (kbd "C-f") 'cargo-process-current-file-tests)
